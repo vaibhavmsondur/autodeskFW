dc5a3ca6e25f4e6286604f9a419db70d2bd4b64a
Vaibhav <vaibhavmsondur@gmail.com>
2022-03-28 15:40:39 +0530

    Retry Retry

--- com.crm.autodeskFrameworkSelenium/src/main/java/com/crm/autodeskFrameworkSelenium/genericUtility/RetryAnalyzerImplementationClass.java
+++ com.crm.autodeskFrameworkSelenium/src/main/java/com/crm/autodeskFrameworkSelenium/genericUtility/RetryAnalyzerImplementationClass.java
@@ -2,6 +2,7 @@ package com.crm.autodeskFrameworkSelenium.genericUtility;
 
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
+
 /**
  * This class is for Retry the Failed Test Scripts using IRetryAnalyzer interface
  * @author VAIBHAV
@@ -11,6 +12,12 @@ public class RetryAnalyzerImplementationClass implements IRetryAnalyzer{
 	int count=0;
 	int retryCount=5;
 	public boolean retry(ITestResult result) {	
+		
+		
+		
+		
+		
+		
 		if (count<retryCount) {
 			count++;
 			
@@ -18,6 +25,7 @@ public class RetryAnalyzerImplementationClass implements IRetryAnalyzer{
 			
 			
 			
+		
 			
 			return true;
 		}
